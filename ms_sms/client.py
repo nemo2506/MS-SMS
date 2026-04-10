4ffa346bc3d6bd83948abf9d4abfe8e7736facf3
MARC NAVARRO <navamarc@gmail.com>
2026-04-10 23:25:08 +0200

    log cleaned

--- ms_sms/client.py
+++ ms_sms/client.py
@@ -110,9 +110,6 @@ class MessagePlugin:
             "base64Jpeg": encode_jpeg_to_base64_max600k(image_path) if image_path else "",
         }
 
-        with open("/home/marc/test_base64.txt", "w", encoding="utf-8") as f:
-            f.write(payload["base64Jpeg"])
-            
         return self._post(payload)
 
     # ------------------------------------------------------------------
